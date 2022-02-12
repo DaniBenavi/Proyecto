6f4beb120de5d6c8c9b2c79164f8365b7138ec92
DaniBenavi <danibenavi1907@gmail.com>
2022-02-11 14:37:51 -1200

    Inicio de proyecto

---
+++ recibe_get.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datos Get</title>
+</head>
+
+<body>
+    Nombre: <?php echo $_GET['nombre'] ?> <br>
+    Usuario: <?php echo $_GET['usuario'] ?> <br>
+    Password: <?php echo $_GET['password'] ?> <br>
+    Password2: <?php echo $_GET['password2'] ?> <br>
+    Email: <?php echo $_GET['email'] ?>
+</body>
+
+</html>
