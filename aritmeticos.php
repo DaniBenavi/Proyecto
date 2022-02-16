e6112bed88d7e1bb141e3f0737122731f0e460bb
DaniBenavi <danibenavi1907@gmail.com>
2022-02-15 22:31:40 -1200

    Operadores aritmeticos

--- aritmeticos.php
+++ aritmeticos.php
@@ -5,7 +5,51 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+
     <title>Recibe aritmeticos</title>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark rounded">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Proyecto</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent" <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+						Clases de programacion web
+					</a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Operadores aritmeticos</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button class="btn btn-outline-success" type="submit">
+						Search
+					</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <br>
 </head>
 
 <body>
@@ -18,22 +62,26 @@
 
     switch ($oper) {
         case '+':
-            echo $valor1 + $valor2;
+            $nombre_op = "Suma";
+            $op =  $valor1 + $valor2;
             break;
 
         case '-':
-            echo $valor1 - $valor2;
+            $nombre_op = "Resta";
+            $op =  $valor1 - $valor2;
             break;
 
         case '*':
-            echo $valor1 * $valor2;
+            $nombre_op = "Multiplicacion";
+            $op = $valor1 * $valor2;
             break;
 
         case '/':
             if ($valor2 == 0) {
                 echo "No se puede dividir entre 0";
             } else {
-                echo  $valor1 / $valor2;
+                $nombre_op = "Division";
+                $op =   $valor1 / $valor2;
             }
             break;
         default:
@@ -41,9 +89,26 @@
             break;
     }
 
-
-
     ?>
+
+    <div class="container-fluid">
+        <table class="table table-hover" border="1">
+            <tbody>
+                <tr>
+                    <th>Operacion</th>
+                    <th>Valor</th>
+                </tr>
+                <tr>
+                    <td><?php echo $nombre_op ?></td>
+                    <td><?php echo $op ?></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
